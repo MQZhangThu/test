6371f899a4b428b7e59f3daff1db176866bf9c51
Meiqing Zhang <ime.ting@hotmail.com>
2015-09-01 11:35:35 +0800

    test

---
+++ index.js
@@ -0,0 +1,35 @@
+var koa = require('koa');
+var app = koa();
+
+function *random(next) {
+  if ('/random' == this.path) {
+    this.body = Math.floor(Math.random()*10);
+  } else {
+    yield next;
+  }
+};
+
+function *backwards(next) {
+  if ('/backwards' == this.path) {
+    this.body = 'sdrawkcab';
+  } else {
+    yield next;
+  }
+}
+
+function *pi(next) {
+  if ('/pi' == this.path) {
+    this.body = String(Math.PI);
+  } else {
+    yield next;
+  }
+}
+
+function *all(next) {
+  yield random.call(this, backwards.call(this, pi.call(this, next)));
+}
+
+app.use(all);
+
+
+app.listen(8081)
